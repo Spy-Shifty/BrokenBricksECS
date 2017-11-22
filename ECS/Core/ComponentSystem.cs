77077b5bebf29a8e2dbfadbe168ab7118aaba355
Spy-Shifty <Spy-Shifty@web.de>
2017-11-22 23:28:03 +0100

    Add files via upload

--- ECS/Core/ComponentSystem.cs
+++ ECS/Core/ComponentSystem.cs
@@ -2,7 +2,9 @@
 
 namespace ECS {
     public class ComponentSystem : IComponentSystemSetup, IEntityAddedEventListener, IEntityRemovedEventListener {
-        public ComponentGroup group { get; private set; } 
+        public ComponentGroup group { get; private set; }
+        
+
         public virtual void OnEntityAdded(Entity entity) { }
         public virtual void OnEntityRemoved(Entity entity) { }
         public virtual void OnStart() { }
@@ -25,6 +27,18 @@ namespace ECS {
             //group.OnEntityAdded -= OnEntityAdded;
             //group.OnEntityRemoved -= OnEntityRemoved;
             group = null;
+        }
+
+        public virtual void OnEntityAdded(object sender, Entity entity) {
+            if (sender == group) {
+                OnEntityAdded(entity);
+            }
+        }
+
+        public virtual void OnEntityRemoved(object sender, Entity entity) {
+            if (sender == group) {
+                OnEntityRemoved(entity);
+            }
         }
     }    
 
