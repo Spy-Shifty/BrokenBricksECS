b81769915e6ab222ad74418b792785d0e9ea03ca
Spy-Shifty <Spy-Shifty@web.de>
2017-11-23 22:27:18 +0100

    BasicEventListeners
    Added classes for faster usage of events through EventListener
    
    E.g. If you want to subscribe to events of multiple groups in the same class you can use this type for easier implementation

---
+++ ECS/Core/BasicEventListener.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ECS {
+    public class EntityAddedEventListener : IEntityAddedEventListener {
+
+        public delegate void EventHandler(Entity entity);
+        private EventHandler _eventHandler;
+
+        public EntityAddedEventListener(EventHandler eventHandler) {
+            if (eventHandler == null) {
+                throw new NullReferenceException();
+            }
+            _eventHandler = eventHandler;
+        }
+
+        public void OnEntityAdded(object sender, Entity entity) {
+            _eventHandler(entity);
+        }
+    }
+
+    public class EntityRemovedEventListener : IEntityRemovedEventListener {
+
+        public delegate void EventHandler(Entity entity);
+        private EventHandler _eventHandler;
+
+        public EntityRemovedEventListener(EventHandler eventHandler) {
+            if (eventHandler == null) {
+                throw new NullReferenceException();
+            }
+            _eventHandler = eventHandler;
+        }
+
+        public void OnEntityRemoved(object sender, Entity entity) {
+            _eventHandler(entity);
+        }
+    }
+
+    public class ComponentAddedToEntityEventListener : IComponentAddedToEntityEventListener {
+
+        public delegate void EventHandler(Entity entity, Type componentType);
+        private EventHandler _eventHandler;
+
+        public ComponentAddedToEntityEventListener(EventHandler eventHandler) {
+            if (eventHandler == null) {
+                throw new NullReferenceException();
+            }
+            _eventHandler = eventHandler;
+        }
+
+        public void OnComponentAddedToEntity(object sender, Entity entity, Type componentType) {
+            _eventHandler(entity, componentType);
+        }
+    }
+
+    public class ComponentRemovedToEntityEventListener : IComponentRemovedFromEntityEventListener {
+
+        public delegate void EventHandler(Entity entity, Type componentType);
+        private EventHandler _eventHandler;
+
+        public ComponentRemovedToEntityEventListener(EventHandler eventHandler) {
+            if (eventHandler == null) {
+                throw new NullReferenceException();
+            }
+            _eventHandler = eventHandler;
+        }
+
+        public void OnComponentRemovedFromEntity(object sender, Entity entity, Type componentType) {
+            _eventHandler(entity, componentType);
+        }
+    }
+
+    public class ComponentAddedToEntityEventListener<TComponent> : IEntityAddedEventListener<TComponent> where TComponent : IComponent {
+
+        public delegate void EventHandler(Entity entity, TComponent component);
+        private EventHandler _eventHandler;
+
+        public ComponentAddedToEntityEventListener(EventHandler eventHandler) {
+            if (eventHandler == null) {
+                throw new NullReferenceException();
+            }
+            _eventHandler = eventHandler;
+        }
+
+        public void OnEntityAdded(object sender, Entity entity, TComponent component) {
+            _eventHandler(entity, component);
+        }
+    }
+
+    public class ComponentRemovedToEntityEventListener<TComponent> : IEntityRemovedEventListener<TComponent> where TComponent : IComponent {
+
+        public delegate void EventHandler(Entity entity, TComponent component);
+        private EventHandler _eventHandler;
+
+        public ComponentRemovedToEntityEventListener(EventHandler eventHandler) {
+            if (eventHandler == null) {
+                throw new NullReferenceException();
+            }
+            _eventHandler = eventHandler;
+        }
+
+        public void OnEntityRemoved(object sender, Entity entity, TComponent component) {
+            _eventHandler(entity, component);
+        }
+    }
+
+    public class ComponentChangedToEntityEventListener<TComponent> : IComponentChangedEventListener<TComponent> where TComponent : IComponent {
+
+        public delegate void EventHandler(Entity entity, TComponent component);
+        private EventHandler _eventHandler;
+
+        public ComponentChangedToEntityEventListener(EventHandler eventHandler) {
+            if (eventHandler == null) {
+                throw new NullReferenceException();
+            }
+            _eventHandler = eventHandler;
+        }
+
+        public void OnComponentChanged(object sender, Entity entity, TComponent component) {
+            _eventHandler(entity, component);
+
+        }
+    }
+}
